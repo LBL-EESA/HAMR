f4f4ee44db531457f03af67ab40066d9a8789a08
Burlen Loring <bloring@lbl.gov>
2021-10-14 13:21:07 -0700

    hamr add runtime environment utility

---
+++ hamr/hamr_env.cxx
@@ -0,0 +1,31 @@
+#include "hamr_env.h"
+
+#include <cstdlib>
+#include <iostream>
+
+namespace hamr
+{
+
+// **************************************************************************
+int get_verbose()
+{
+    static int ival = -1;
+
+    if (ival < 0)
+    {
+        char *cval = getenv("HAMR_VERBOSE");
+        if (cval)
+        {
+            ival = atoi(cval);
+            std::cerr << "HAMR_VERBOSE=" << ival << std::endl;
+        }
+        else
+        {
+            ival = 0;
+        }
+    }
+
+    return ival;
+}
+
+}
---
+++ hamr/hamr_env.h
@@ -0,0 +1,13 @@
+#ifndef hamr_env_h
+#define hamr_env_h
+
+/// heterogeneous accelerator memory resource
+namespace hamr
+{
+
+/// returns the value of the HAMR_VERBOSE environment variable
+int get_verbose();
+
+}
+
+#endif
