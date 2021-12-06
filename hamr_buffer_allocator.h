38eda160864edce8b8845072d9049a9c4c273ef7
Burlen Loring <bloring@lbl.gov>
2021-12-06 10:56:56 -0800

    rtd fix buffer_allocator doxygen

--- hamr_buffer_allocator.h
+++ hamr_buffer_allocator.h
@@ -9,12 +9,12 @@ namespace hamr
 /// allocator types that may be used with hamr::buffer
 enum class buffer_allocator
 {
-    same = -2,   /// propagate the current allocator
-    none = -1,   /// no allocator specified
-    cpp = 0,     /// allocates memory with new
-    malloc = 1,  /// allocates memory with malloc
-    cuda = 2,    /// allocates memory with cudaMalloc
-    cuda_uva = 3 /// allocates memory with cudaMallocManaged
+    same = -2,   ///< propagate the current allocator
+    none = -1,   ///< no allocator specified
+    cpp = 0,     ///< allocates memory with new
+    malloc = 1,  ///< allocates memory with malloc
+    cuda = 2,    ///< allocates memory with cudaMalloc
+    cuda_uva = 3 ///< allocates memory with cudaMallocManaged
 };
 
 }
