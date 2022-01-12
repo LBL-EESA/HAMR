5843aab2537e487ecf0525b73430a3b1eb6ff780
Burlen Loring <bloring@lbl.gov>
2022-01-12 09:21:45 -0800

    python expose hamr::buffer zero-copy constructor

---
+++ python/hamr_python_deleter.h
@@ -0,0 +1,75 @@
+#ifndef hamr_python_deleter_h
+#define hamr_python_deleter_h
+
+#include <Python.h>
+#include <iostream>
+
+namespace hamr
+{
+
+/// a deleter for memory managed from within Python
+/** This class manages an array allocated by a Python code. In the functor's
+ * constructor a refrence to a user provdied Python object is stolen. When the
+ * functor is invoked, a reference to this Python object is released. It is up
+ * to the Python object to free the memory. One may use a PyCapsule to
+ * implement custom delete methods if they are needed.
+ */
+template <typename T>
+class HAMR_EXPORT python_deleter
+{
+public:
+    /** constructs the deleter. A reference to obj is stolen by this constructor.
+     * @param[in] obj a PyObject who's reference count will be decremented when
+     *                the data shared from Python is no longer needed.
+     */
+    python_deleter(T *ptr, size_t n_elem, PyObject *obj);
+
+    /** deletes the array
+     * @param[in] ptr the pointer to the array to delete. must be the same as
+     *                that passed during construction.
+     */
+    void operator()(T *ptr);
+
+private:
+    T *m_ptr;
+    size_t m_elem;
+    PyObject *m_object;
+};
+
+// --------------------------------------------------------------------------
+template <typename T>
+python_deleter<T>::python_deleter(T *ptr, size_t n, PyObject *obj)
+    : m_ptr(ptr), m_elem(n), m_object(obj)
+{
+#if defined(HAMR_VERBOSE)
+    if (hamr::get_verbose())
+    {
+        std::cerr << "created python_deleter for array of " << n
+            << " objects of type " << typeid(T).name() << sizeof(T)
+            << " holding a reference to " << m_object << std::endl;
+    }
+#endif
+
+    Py_INCREF(obj);
+}
+
+// --------------------------------------------------------------------------
+template <typename T>
+void python_deleter<T>::operator()(T *ptr)
+{
+    assert(ptr == m_ptr);
+
+#if defined(HAMR_VERBOSE)
+    if (hamr::get_verbose())
+    {
+        std::cerr << "python_deleter deleting array of " << m_elem
+            << " objects of type " << typeid(T).name() << sizeof(T)
+            << " release reference to " << m_object << std::endl;
+    }
+#endif
+
+    Py_DECREF(m_object);
+}
+
+}
+#endif
--- test/test_hamr_buffer_cupy_cuda.py
+++ test/test_hamr_buffer_cupy_cuda.py
@@ -1,39 +1,84 @@
 from hamr import *
-import numpy as np
 import cupy as cp
 import sys
 
 stderr = sys.__stderr__
 
-stderr.write('TEST: creating a hamr::buffer w. CUDA ... \n')
-buf = buffer_float(buffer_allocator_cuda, 16, 3.1415)
+n_elem = 256
+init_val = 3.1415
+mod_val = 10000
+res_val = init_val*mod_val
+
+# send data from C++ to Python
+stderr.write('TEST 1 : C++ --> Python\n' \
+             '=======================\n')
+
+stderr.write('TEST 1: creating a hamr::buffer w. CUDA ... \n')
+buf = buffer_float(buffer_allocator_cuda, n_elem, init_val)
 stderr.write('buf = %s\n'%(str(buf)))
-stderr.write('TEST: creating a hamr::buffer w. CUDA ... OK!\n\n')
+stderr.write('TEST 1: creating a hamr::buffer w. CUDA ... OK!\n\n')
 
-stderr.write('TEST: get a handle to the data ... \n')
+stderr.write('TEST 1: get a handle to the data ... \n')
 h = buf.get_cuda_accessible()
-stderr.write('TEST: get a handle to the data ... OK!\n\n')
+stderr.write('TEST 1: get a handle to the data ... OK!\n\n')
 
-stderr.write('TEST: share the data with Cupy ... \n')
+stderr.write('TEST 1: share the data with Cupy ... \n')
 arr = cp.array(h, copy=False)
 stderr.write('arr.__cuda_array_interface__ = %s\n'%(arr.__cuda_array_interface__))
-stderr.write('TEST: share the data with Cupy ... OK!\n\n')
+stderr.write('TEST 1: share the data with Cupy ... OK!\n\n')
 
-stderr.write('TEST: deleting the hamr::buffer ... \n')
+stderr.write('TEST 1: deleting the hamr::buffer ... \n')
 buf = None
-stderr.write('TEST: deleting the hamr::buffer ... OK!\n\n')
+h = None
+stderr.write('TEST 1: deleting the hamr::buffer ... OK!\n\n')
 
-stderr.write('TEST: Cupy reads the data ... \n')
+stderr.write('TEST 1: Cupy modifies the data ... \n')
+arr *= mod_val
 stderr.write('arr = %s\n'%(str(arr)))
-stderr.write('TEST: Cupy reads the data ... OK!\n\n')
+stderr.write('TEST 1: Cupy modifies the data ... OK!\n\n')
 
-stderr.write('TEST: Cupy modifies the data ... \n')
-arr *= 10000
-stderr.write('arr = %s\n'%(str(arr)))
-stderr.write('TEST: Cupy modifies the data ... OK!\n\n')
+stderr.write('TEST 1: Verify the result ... \n')
+if not cp.allclose(arr, res_val):
+    stderr.write('ERROR: TEST 1 failed!\n')
+    sys.exit(-1)
+stderr.write('TEST 1: Verify the result ... OK\n\n')
 
-stderr.write('TEST: deleting the Cupy array ... \n')
+stderr.write('TEST 1: deleting the Cupy array ... \n')
 arr = None
-stderr.write('TEST: deleting the Cupy array ... OK!\n\n')
+stderr.write('TEST 1: deleting the Cupy array ... OK!\n\n')
+
+
+
+# send data from Python to C++
+stderr.write('TEST 2 : Python --> C++\n' \
+             '=======================\n')
+
+stderr.write('TEST 2: creating a Cupy array ... \n')
+arr = cp.full((n_elem), init_val, dtype='float32')
+stderr.write('arr.__cuda_array_interface__ = %s\n'%(arr.__cuda_array_interface__))
+#stderr.write('arr = %s\n'%(str(arr)))
+stderr.write('TEST 2: creating a Cupy array ... OK\n\n')
+
+stderr.write('TEST 2: share the data with hamr::buffer ... \n')
+buf = buffer(arr)
+stderr.write('buf = %s\n'%(str(buf)))
+stderr.write('TEST 2: share the data with hamr::buffer ... OK\n\n')
+
+stderr.write('TEST 2: Cupy modifies the data ... \n')
+arr *= mod_val
+#stderr.write('arr = %s\n'%(str(arr)))
+stderr.write('TEST 2: Cupy modifies the data ... OK!\n\n')
+
+stderr.write('TEST 2: deleting the Cupy array ... \n')
+arr = None
+stderr.write('TEST 2: deleting the Cupy array ... OK!\n\n')
+
+stderr.write('TEST 2: display the modified hamr::buffer ... \n')
+stderr.write('buf = %s\n'%(str(buf)))
+stderr.write('TEST 2: display the modified hamr::buffer ... OK\n\n')
+
+stderr.write('TEST 2: deleting the hamr::buffer ... \n')
+buf = None
+stderr.write('TEST 2: deleting the hamr::buffer ... OK!\n\n')
 
 sys.exit(0)
