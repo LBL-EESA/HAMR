254800aa5097d6557295840b7ebe65101c2b1495
Burlen Loring <bloring@lbl.gov>
2022-01-26 13:13:11 -0800

    doc simplify hello cuda example

--- doc/rtd/source/hello_cuda/add_cuda_dispatch.h
+++ doc/rtd/source/hello_cuda/add_cuda_dispatch.h
@@ -4,7 +4,6 @@
 #include "add_cuda.h"
 
 #include <hamr_buffer.h>
-#include <hamr_cuda_launch.h>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
@@ -12,48 +11,32 @@
 #include <iostream>
 
 using hamr::buffer;
-using hamr::p_buffer;
 using allocator = hamr::buffer_allocator;
 
 // **************************************************************************
 template <typename T, typename U>
-p_buffer<T> add_cuda(const p_buffer<T> &a1, const p_buffer<U> &a2)
+buffer<T> add_cuda(const buffer<T> &a1, const buffer<U> &a2)
 {
-    // get the inputs
-    auto spa1 = a1->get_cuda_accessible();
+    // get pointers to the input arrays
+    auto spa1 = a1.get_cuda_accessible();
     const T *pa1 = spa1.get();
 
-    auto spa2 = a2->get_cuda_accessible();
+    auto spa2 = a2.get_cuda_accessible();
     const U *pa2 = spa2.get();
 
-    // allocate the memory
-    size_t n_vals = a1->size();
-    p_buffer<T> ao = std::make_shared<buffer<T>>(allocator::cuda);
-    ao->resize(n_vals, T(0));
+    // allocate the memory for the result
+    size_t n_vals = a1.size();
+    buffer<T> ao(allocator::cuda, n_vals, T(0));
 
-    auto spao = ao->get_cuda_accessible();
+    // get pointer to the output array
+    auto spao = ao.get_cuda_accessible();
     T *pao = spao.get();
 
-    // determine kernel launch parameters
-    int n_blocks = 0;
-    dim3 block_grid;
-    dim3 thread_grid;
-    if (hamr::partition_thread_blocks(0, n_vals,
-        8, block_grid, n_blocks, thread_grid))
-    {
-        std::cerr << "ERROR: Failed to determine launch parameters" << std::endl;
-        return nullptr;
-    }
+    // determine kernel launch parameters and launch the kernel to add the arrays
+    dim3 thread_grid(128);
+    dim3 block_grid(n_vals/128 + (n_vals % 128 ? 1 : 0));
 
-    // initialize the data
-    cudaError_t ierr = cudaSuccess;
     add_cuda<<<block_grid, thread_grid>>>(pao, pa1, pa2, n_vals);
-    if ((ierr = cudaGetLastError()) != cudaSuccess)
-    {
-        std::cerr << "ERROR: Failed to launch the add_cuda kernel. "
-            << cudaGetErrorString(ierr) << std::endl;
-        return nullptr;
-    }
 
     return ao;
 }
--- doc/rtd/source/hello_cuda/hello_cuda.cu
+++ doc/rtd/source/hello_cuda/hello_cuda.cu
@@ -4,26 +4,28 @@
 #include <iostream>
 #include <memory>
 
+using hamr::buffer;
+using allocator = hamr::buffer_allocator;
+
 int main(int, char **)
 {
     size_t n_vals = 400;
 
     // allocate an array initialized to 1 on the GPU
-    auto a0 = std::make_shared<buffer<float>>(allocator::cuda, n_vals, 1.0f);
+    buffer<float> a0(allocator::cuda, n_vals, 1.0f);
 
     // allocate an array initialized to 1 on the CPU
-    auto a1 = std::make_shared<buffer<float>>(allocator::malloc, n_vals, 1.0f);
+    buffer<float> a1(allocator::malloc, n_vals, 1.0f);
 
     // add the two arrays on the GPU
-    auto a2 = add_cuda(a0, a1);
-
-    // access the result on the CPU
-    auto spa2 = a2->get_cpu_accessible();
-    float *pa2 = spa2.get();
+    buffer<float> a2 = add_cuda(a0, a1);
 
     // print the result on the CPU
+    auto spa2 = a2.get_cpu_accessible();
+    float *pa2 = spa2.get();
+
     std::cerr << "a2 = ";
-    for (int i = 0; i < a2->size(); ++i)
+    for (int i = 0; i < a2.size(); ++i)
         std::cerr << pa2[i] << " ";
     std::cerr << std::endl;
 
