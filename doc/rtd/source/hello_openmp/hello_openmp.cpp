d7d10bb48b767eee883defd2e3da13b7cc825d6f
Burlen Loring <bloring@lbl.gov>
2022-04-01 12:23:15 -0700

    doc rtd user guide document using OpenMP

---
+++ doc/rtd/source/hello_openmp/hello_openmp.cpp
@@ -0,0 +1,26 @@
+#include <hamr_buffer.h>
+
+#include <iostream>
+#include <memory>
+
+#include "add.h"
+#include "write.h"
+
+int main(int, char **)
+{
+    size_t n_vals = 400;
+
+    // allocate and initialize to 1 on the GPU
+    hamr::buffer<float> a0(hamr::buffer_allocator::openmp, n_vals, 1.0f);
+
+    // allocate and initialize to 1 on the CPU
+    hamr::buffer<float> a1(hamr::buffer_allocator::malloc, n_vals, 1.0f);
+
+    // add the two arrays
+    hamr::buffer<float> a2 = add(a0, a1);
+
+    // write the result
+    write(std::cerr, a2);
+
+    return 0;
+}
